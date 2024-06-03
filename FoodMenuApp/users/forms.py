80e41101d2a376cdcbb4ccbfbef04bd8a40cc763
Romjan Ali <romjanvr5@gmail.com>
2024-06-03 20:00:02 +0600

    Authentication view added

--- FoodMenuApp/users/forms.py
+++ FoodMenuApp/users/forms.py
@@ -5,7 +5,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
+    description = forms.CharField(max_length= 250)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'description']
+
+        help_text = {
+            'username': None,
+            'password1': None
+        }
--- FoodMenuApp/users/views.py
+++ FoodMenuApp/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import redirect
 from users.forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 
 
 # Create your views here.
@@ -11,9 +13,48 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
+            print(form.cleaned_data)
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username} your account has successfully created')
             return redirect('food:index')
         
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_user(request):
+    form = AuthenticationForm()
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            messages.error(request, "Username and password do not match")
+            return render(request, 'users/login.html', {'form': form})
+
+        login(request, user)
+        messages.success(request, f'Welcome {username} login successfull')
+        return redirect('food:index')
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_user(request):
+    username = request.user
+    messages.success(request, f'{username} logged out successfully')
+    logout(request)
+    return redirect("food:index")
+
+def pass_change(request):
+    form = PasswordChangeForm(request.user)
+
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.cleaned_data['user'])
+            return redirect('profile')
+    return render(request, 'users/pass_change.html', {'form': form})
+
